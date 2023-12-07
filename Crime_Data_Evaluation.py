d1573c65e1cf81527413dca82e189b7ccc348ae7
Tim <gernetim@gmail.com>
2023-12-07 10:20:31 +0100

    First overview of the PKS data

---
+++ Crime_Data_Evaluation.py
@@ -0,0 +1,52 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+wd = os.getcwd()
+os.chdir(wd + '/Datasets/PKS/2022/Zeitliche-Gliederung')
+
+# In the csv file commas are used to structure large numbers
+# dots are used as seperator for decimal numbers eg 3.5%
+data = pd.read_csv('T01-Faelle.csv', encoding='latin_1', skiprows=0, header=1,
+                   sep=';', decimal='.', thousands=',')
+
+# Get an overview how the crimes are distinguished
+# crimes = []
+# for crime in list(data['Straftat']):
+#    if crime not in crimes:
+#         crimes.append(crime)
+#
+# print(crimes)
+# print('Number of different categories of crimes: %i' % len(crimes))
+
+# Get an overview of the crime keys
+keys = []
+for key in list(data['Schluessel']):
+    if key not in keys:
+        keys.append(key)
+
+print(keys)
+print(len(keys))
+
+# Create new DataFrames with different crimes
+# TODO: CSV file Problem: bei den Schluesseln sind die leading 0 entfernt -> fixen
+Straf_geg_Leben = data[data['Schluessel'].str.startswith('0')]
+print(Straf_geg_Leben.head())
+
+
+# Print some crimes
+# relevant_crimes = ['Straftaten gegen das Leben', 'Mord § 211 StGB darunter:',
+#               'Alle übrigen (vorsätzlichen) Tötungen §§ 212, 213, 216, 217 StGB']
+# crimes
+# for crime in relevant_crimes:
+#     data_crime = data.loc[data['Straftat'] == crime]
+#     years = list(data_crime.loc[:, 'Jahr'])
+#     cases = list(data_crime.loc[:, 'erfasste Faelle'])
+#     # cases = [eval(case) for case in cases]
+#     print(crime, str(years))
+#     plt.plot(years, cases)
+
+# plt.yscale('log')
+# plt.legend(relevant_crimes, loc='upper right')
+# plt.show()
