2cdc9b67c16aca1bcd7c07a896480c5d176861d5
Tim <gernetim@gmail.com>
2023-12-20 15:16:11 +0100

    Added Plot title

--- Crime_Data_Evaluation.py
+++ Crime_Data_Evaluation.py
@@ -6,13 +6,7 @@ import pandas as pd
 wd = os.getcwd()
 os.chdir(wd + '/Datasets/PKS/2022/Zeitliche-Gliederung')
 
-# In the csv file commas are used to structure large numbers
-# dots are used as seperator for decimal numbers eg 3.5%
-# data = pd.read_csv('T01-Faelle.csv', encoding='latin_1', skiprows=0, header=1,
-#                    sep=';', decimal='.', thousands=',')
-
 data = pd.read_excel('T01-Faelle.xlsx', skiprows=14, header=0, decimal='.', thousands=',')
-
 
 # Get an overview how the crimes are distinguished
 # crimes = []
@@ -45,14 +39,38 @@ for old_name, new_name in zip(data.columns, new_names):
 print(data.head())
 
 
-
 # Summarize/Categorize Crimes in new DataFrames
-straf_geg_leben = data[data['Schluessel'].str.startswith('0')]
-print(len(straf_geg_leben))
+def get_crimes(key_val, key_col='Schluessel'):
+    crime_df = data[data[key_col].str.startswith(key_val)]
+    return crime_df
 
-print(straf_geg_leben.head())
+
+# TODO: Genaue Kategorisierung ueberlgen und durchfuehren
+straftaten_gegen_leben = get_crimes('0')
+rauschgiftdelikte_nach_BtMG = get_crimes('73')
+
+print(rauschgiftdelikte_nach_BtMG.head())
 
 
+crime_of_interest = get_crimes('0')
+years = list(crime_of_interest['Jahr'])
+cases = list(crime_of_interest['erfasste Faelle'])
+
+diff_crimes = []
+for crime in crime_of_interest['Straftat']:
+    if crime not in diff_crimes:
+        diff_crimes.append(crime)
+
+
+
+sum_crimes = crime_of_interest.groupby('Jahr')['erfasste Faelle'].sum().reset_index()
+sum_crimes_cases = list(sum_crimes['erfasste Faelle'])
+sum_crimes_years = list(sum_crimes['Jahr'])
+
+plt.plot(years, cases)
+#plt.plot(sum_crimes_years, sum_crimes_cases, color='red')
+plt.legend(diff_crimes)
+plt.show()
 
 # Print some crimes
 # relevant_crimes = ['Straftaten gegen das Leben', 'Mord § 211 StGB darunter:',
