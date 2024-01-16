09c80b9c67781c3a04f6a0205714d449cc311650
Lukas Weber <LukasWeber2261@web.de>
2024-01-16 14:22:31 +0100

    add file for variables

---
+++ src/variables.py
@@ -0,0 +1,5 @@
+"""
+This class is purely for defining variables that we will use often.
+"""
+
+months = ['Jan.', 'Febr.', 'März', 'April', 'Mai', 'Juni', 'Juli', 'Aug.', 'Sept.', 'Okt.', 'Nov.', 'Dez.']
